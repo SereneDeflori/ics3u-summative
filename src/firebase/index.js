998638c2f71d0c4758003e592bed974f56e8798f
Serene Deflorimonte <Serene.deflorimonte@student.tdsb.on.ca>
2024-12-10 13:54:04 -0500

    yas

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,6 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAOSlUtUpWLiT2a4W7L6MOiUnW3ANj81R0",
+    authDomain: "summative-a4ef2.firebaseapp.com",
+    projectId: "summative-a4ef2",
+    storageBucket: "summative-a4ef2.firebasestorage.app",
+    messagingSenderId: "797337643655",
+    appId: "1:797337643655:web:3552539661961fb3e02622"
+  };
+  
 // Paste your firebaseConfig from Firebase Console here
 
 
